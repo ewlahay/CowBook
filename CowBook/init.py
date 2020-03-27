d910accea0c7271a4cc2b2c74a79aac7c4a0714c
Emmett <ewlahay@ncsu.edu>
2020-03-27 01:01:08 -0400

    Fix running app without environmental variables defined

--- CowBook/init.py
+++ CowBook/init.py
@@ -8,7 +8,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config.from_object('CowBook.default_settings')
-app.config.from_pyfile(os.environ['APP_CONFIG'])
+#app.config.from_pyfile(os.environ['APP_CONFIG'])
+try:
+	app.config.from_envvar('APP_CONFIG')
+except RuntimeError:
+	print("Environmental variable APP_CONFIG not set!")
+except FileNotFoundError:
+	print("Config file not found!")
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 nav = Nav()
