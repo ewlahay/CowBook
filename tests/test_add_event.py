3500da88a54932670e39a4dcf3ed775382eaff70
Emmett <ewlahay@ncsu.edu>
2021-10-21 17:52:58 -0400

    Update dependencies, fix failing test

--- tests/test_add_event.py
+++ tests/test_add_event.py
@@ -7,18 +7,20 @@ from tests.helpers import login, add_cow
 from CowBook.Models.Cow.CowModel import get_by_id
 
 
-def add_event(client, event=None, cow=1):
+def add_event(client, event=None, cow=1, bredDate: datetime = datetime.now()):
 	add_cow(client)
 	if event is None:
-		event = {"formType": "Bred", "date": "2020-10-19", "sire": "Big Mac", "notes": "Calving ease bull"}
+		dateStr = bredDate.strftime("%Y-%m-%d")
+		print(dateStr)
+		event = {"formType": "Bred", "date": dateStr, "sire": "Big Mac", "notes": "Calving ease bull"}
 	client.post("/herd/{}/addEvent".format(cow), data=event, follow_redirects=True)
 
 
 def test_add_event(client):
-	add_event(client)
 	start = datetime(2020, 10, 19, tzinfo=pytz.UTC)
+	add_event(client, bredDate=start)
 	end = start + timedelta(weeks=52)
-	url = "api/herd/duedates?start=2020-10-19T00%3A00%3A00-05%3A00&end=2022-02-07T00%3A00%3A00-05%3A00"
+	url = "api/herd/duedates?start=2019-10-19T00%3A00%3A00-05%3A00&end=2022-02-07T00%3A00%3A00-05%3A00"
 	# print(url)
 	rv = client.get(url)
 	data = json.loads(rv.data)[0]
